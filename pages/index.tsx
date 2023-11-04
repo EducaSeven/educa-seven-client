59b31c995963168a5423ea00e9391e646d6c92f5
Murilo Marçal de Araujo <102617690+Murilo-Araujo@users.noreply.github.com>
2023-11-04 19:44:28 -0300

    creating list

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import {Inter} from 'next/font/google'
+import DashboardTable from "@/components/dashboard_table";
+import Title from "@/components/title";
 
 
 const inter = Inter({subsets: ['latin']})
 
 export default function Home() {
     return (
-        <h1>
-            osdasdasd
-        </h1>
+        <div className={'p-12'}>
+            <Title>Dashboard</Title>
+
+            <DashboardTable className={'mt-12'}/>
+        </div>
     )
 }

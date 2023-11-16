208305e0e25b3c403bb1bf6423a5665031858971
Murilo Marçal de Araujo <102617690+Murilo-Araujo@users.noreply.github.com>
2023-11-15 21:47:47 -0300

    sidebar

--- pages/create_quiz.tsx
+++ pages/create_quiz.tsx
@@ -1,303 +1,304 @@
-import { useRouter } from "next/router";
-import { useState, useEffect } from "react";
+import {useRouter} from "next/router";
+import {useState, useEffect} from "react";
 
 export default function CreateQuiz() {
-  const router = useRouter();
-  const { quesId } = router.query;
+    const router = useRouter();
+    const {quesId} = router.query;
 
-  useEffect(() => {
-    if (quesId) {
-      console.log("ID do questionário:", quesId);
+    useEffect(() => {
+        if (quesId) {
+            console.log("ID do questionário:", quesId);
+        }
+    }, [quesId]);
+
+    interface Questionario {
+        quesId: number;
+        quesTittle: string;
+        quesDescription: string;
+        quesImage: string;
+        perguntas: Pergunta[];
     }
-  }, [quesId]);
 
-  interface Questionario {
-    quesId: number;
-    quesTittle: string;
-    quesDescription: string;
-    quesImage: string;
-    perguntas: Pergunta[];
-  }
+    interface Pergunta {
+        pergId: number;
+        quesId: number;
+        pergTittle: string;
+        pergDescription: string;
+        respostas: Resposta[];
+    }
 
-  interface Pergunta {
-    pergId: number;
-    quesId: number;
-    pergTittle: string;
-    pergDescription: string;
-    respostas: Resposta[];
-  }
+    interface Resposta {
+        respId: number;
+        pergId: number;
+        respDescription: string;
+        respResposta: boolean;
+    }
 
-  interface Resposta {
-    respId: number;
-    pergId: number;
-    respDescription: string;
-    respResposta: boolean;
-  }
+    interface Toast {
+        description: string;
+        type: string;
+    }
 
-  interface Toast {
-    description: string;
-    type: string;
-  }
 
-  const [toast, setToast] = useState<Toast[]>([{ description: "", type: "" }]);
+    const [toast, setToast] = useState<Toast[]>([]);
 
-  function toastEmitted(descriptions: string[], type: string = "warning") {
-    const newToasts = descriptions.map((description) => ({
-      description,
-      type,
-    }));
-    setToast((prevToasts) => [...prevToasts, ...newToasts]);
-    automaticallyDeleteToast(newToasts.length);
-  }
+    function toastEmitted(descriptions: string[], type: string = "warning") {
+        const newToasts = descriptions.map((description) => ({
+            description,
+            type,
+        }));
+        setToast((prevToasts) => [...prevToasts, ...newToasts]);
+        automaticallyDeleteToast(newToasts.length);
+    }
 
-  function automaticallyDeleteToast(numberOfToasts: number) {
-    setTimeout(() => {
-      setToast((prevToasts) => prevToasts.slice(numberOfToasts));
-    }, 5000);
-  }
+    function automaticallyDeleteToast(numberOfToasts: number) {
+        setTimeout(() => {
+            setToast((prevToasts) => prevToasts.slice(numberOfToasts));
+        }, 5000);
+    }
 
-  function clearToast() {
-    setToast((prevToasts) => prevToasts.slice(1));
-  }
+    function clearToast() {
+        setToast((prevToasts) => prevToasts.slice(1));
+    }
 
-  const [providerTablePerguntas, setProviderTablePerguntas] = useState<
-    Pergunta[]
-  >([]);
+    const [providerTablePerguntas, setProviderTablePerguntas] = useState<
+        Pergunta[]
+    >([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const data: Pergunta[] = await getPergunta();
-      setProviderTablePerguntas(data);
-    };
+    useEffect(() => {
+        const fetchData = async () => {
+            const data: Pergunta[] = await getPergunta();
+            setProviderTablePerguntas(data);
+        };
 
-    fetchData();
-  }, []);
+        fetchData();
+    }, []);
 
-  const [quiz, setQuiz] = useState<Questionario>({
-    quesId: 0,
-    quesTittle: "",
-    quesDescription: "",
-    quesImage: "",
-    perguntas: [],
-  });
+    const [quiz, setQuiz] = useState<Questionario>({
+        quesId: 0,
+        quesTittle: "",
+        quesDescription: "",
+        quesImage: "",
+        perguntas: [],
+    });
 
-  const [quizTitle, setQuizTitle] = useState("");
-  const [quizDescription, setQuizDescription] = useState("");
-  const [quizImage, setQuizImage] = useState("");
+    const [quizTitle, setQuizTitle] = useState("");
+    const [quizDescription, setQuizDescription] = useState("");
+    const [quizImage, setQuizImage] = useState("");
 
-  let providerPerguntas: Pergunta[] = getPergunta();
-  let pergunta: Pergunta;
+    let providerPerguntas: Pergunta[] = getPergunta();
+    let pergunta: Pergunta;
 
-  function setPergunta(value: number) {
-    if (value === 0) return;
+    function setPergunta(value: number) {
+        if (value === 0) return;
 
-    const selectedPergunta = providerPerguntas.find(
-      (question) => question.pergId === value
+        const selectedPergunta = providerPerguntas.find(
+            (question) => question.pergId === value
+        );
+
+        if (selectedPergunta) {
+            pergunta = selectedPergunta;
+        }
+    }
+
+    function onAdd() {
+        if (pergunta) {
+            setProviderTablePerguntas((prevPerguntas) => [
+                ...prevPerguntas,
+                pergunta,
+            ]);
+        }
+    }
+
+    function getPergunta() {
+        let perguntas: Pergunta[] = [];
+
+        const pergunta1 = {
+            pergId: 1,
+            quesId: 1,
+            pergTittle: "Primeira pergunta",
+            pergDescription: "Primeira pergunta",
+            respostas: [
+                {
+                    respId: 1,
+                    pergId: 1,
+                    respDescription: "Primeira resposta",
+                    respResposta: true,
+                },
+                {
+                    respId: 2,
+                    pergId: 1,
+                    respDescription: "Segunda resposta",
+                    respResposta: false,
+                },
+            ],
+        };
+
+        perguntas.push(pergunta1);
+
+        const pergunta2 = {
+            pergId: 2,
+            quesId: 1,
+            pergTittle: "Segunda pergunta",
+            pergDescription: "Segunda pergunta",
+            respostas: [
+                {
+                    respId: 1,
+                    pergId: 2,
+                    respDescription: "Primeira resposta",
+                    respResposta: true,
+                },
+                {
+                    respId: 2,
+                    pergId: 2,
+                    respDescription: "Segunda resposta",
+                    respResposta: false,
+                },
+            ],
+        };
+
+        perguntas.push(pergunta2);
+
+        return perguntas;
+    }
+
+    function onSubmit() {
+        const quesIdNumber = quesId
+            ? parseInt(Array.isArray(quesId) ? quesId[0] : quesId, 10)
+            : 0;
+
+        const newQuiz = {
+            quesId: quesIdNumber,
+            quesTittle: quizTitle,
+            quesDescription: quizDescription,
+            quesImage: quizImage,
+            perguntas: providerTablePerguntas,
+        };
+
+        if (newQuiz) {
+            setQuiz(newQuiz);
+            console.log(newQuiz);
+            toastEmitted(["Quiz criado com sucesso!"], "success");
+        } else {
+            toastEmitted(["Erro ao criar quiz"], "warning");
+        }
+    }
+
+    function onDeletePergunta(pergunta: Pergunta) {
+        return () => {
+            const updatedPerguntas = providerTablePerguntas.filter(
+                (question) => question.pergId !== pergunta.pergId
+            );
+            setProviderTablePerguntas(updatedPerguntas);
+        };
+    }
+
+    return (
+        <div className="w-full px-14 flex flex-col gap-6 mt-6 h-full">
+            <div
+                className="toast toast-top toast-end z-50 cursor-pointer"
+                onClick={clearToast}
+            >
+                {toast.map((toast, index) => (
+                    <div
+                        key={index}
+                        className={`alert ${toast.type == "warning" ? "alert-info" : ""} ${
+                            toast.type == "success" ? "alert-success" : ""
+                        }`}
+                    >
+                        <span>{toast.description}</span>
+                    </div>
+                ))}
+            </div>
+            <div className="flex justify-between items-center">
+                <h1 className="text-2xl font-bold">Criar novo Quiz</h1>
+                <button className="btn btn-primary" onClick={onSubmit}>
+                    Salvar
+                </button>
+            </div>
+            <div className="w-full flex justify-between align-center gap-12">
+                <div className="form-control w-full max-w-xl ">
+                    <label className="label">
+                        <span className="label-text">Título</span>
+                    </label>
+                    <input
+                        value={quizTitle}
+                        onChange={(e) => setQuizTitle(e.target.value)}
+                        type="text"
+                        className="input input-bordered w-full max-w-xl"
+                    />
+                </div>
+                <div className="form-control w-full max-w-md self-end">
+                    <input
+                        value={quizImage}
+                        onChange={(e) => setQuizImage(e.target.value)}
+                        type="file"
+                        className="file-input file-input-bordered w-full max-w-md"
+                    />
+                </div>
+            </div>
+            <div className="form-control">
+                <label className="label">
+                    <span className="label-text">Descrição</span>
+                </label>
+                <textarea
+                    value={quizDescription}
+                    onChange={(e) => setQuizDescription(e.target.value)}
+                    className="textarea textarea-bordered h-24"
+                ></textarea>
+            </div>
+            <div className="flex flex-row gap-6">
+                <div className="form-control w-full">
+                    <label className="label">
+                        <span className="label-text">Pergunta</span>
+                    </label>
+                    <select
+                        onChange={(e) => setPergunta(parseInt(e.target.value, 10))}
+                        className="select select-bordered"
+                        defaultValue="0"
+                    >
+                        <option value={0} disabled selected>
+                            Selecione uma pergunta
+                        </option>
+                        {providerPerguntas.map((question) => (
+                            <option value={question.pergId} key={question.pergId}>
+                                {question.pergTittle}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+                <button className="btn btn-primary self-end" onClick={onAdd}>
+                    Adicionar
+                </button>
+            </div>
+            <div className="overflow-auto">
+                <table className="table">
+                    <thead>
+                    <tr>
+                        <th></th>
+                        <th>Nome</th>
+                        <th>Descrição</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {providerTablePerguntas.map((question) => (
+                        <tr key={question.pergId}>
+                            <th>{question.pergId}</th>
+                            <td>{question.pergTittle}</td>
+                            <td>{question.pergDescription}</td>
+                            <th className="flex justify-end gap-2">
+                                <button
+                                    className="btn btn-error btn-xs"
+                                    onClick={onDeletePergunta(question)}
+                                >
+                                    delete
+                                </button>
+                            </th>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
     );
-
-    if (selectedPergunta) {
-      pergunta = selectedPergunta;
-    }
-  }
-
-  function onAdd() {
-    if (pergunta) {
-      setProviderTablePerguntas((prevPerguntas) => [
-        ...prevPerguntas,
-        pergunta,
-      ]);
-    }
-  }
-
-  function getPergunta() {
-    let perguntas: Pergunta[] = [];
-
-    const pergunta1 = {
-      pergId: 1,
-      quesId: 1,
-      pergTittle: "Primeira pergunta",
-      pergDescription: "Primeira pergunta",
-      respostas: [
-        {
-          respId: 1,
-          pergId: 1,
-          respDescription: "Primeira resposta",
-          respResposta: true,
-        },
-        {
-          respId: 2,
-          pergId: 1,
-          respDescription: "Segunda resposta",
-          respResposta: false,
-        },
-      ],
-    };
-
-    perguntas.push(pergunta1);
-
-    const pergunta2 = {
-      pergId: 2,
-      quesId: 1,
-      pergTittle: "Segunda pergunta",
-      pergDescription: "Segunda pergunta",
-      respostas: [
-        {
-          respId: 1,
-          pergId: 2,
-          respDescription: "Primeira resposta",
-          respResposta: true,
-        },
-        {
-          respId: 2,
-          pergId: 2,
-          respDescription: "Segunda resposta",
-          respResposta: false,
-        },
-      ],
-    };
-
-    perguntas.push(pergunta2);
-
-    return perguntas;
-  }
-
-  function onSubmit() {
-    const quesIdNumber = quesId
-      ? parseInt(Array.isArray(quesId) ? quesId[0] : quesId, 10)
-      : 0;
-
-    const newQuiz = {
-      quesId: quesIdNumber,
-      quesTittle: quizTitle,
-      quesDescription: quizDescription,
-      quesImage: quizImage,
-      perguntas: providerTablePerguntas,
-    };
-
-    if (newQuiz) {
-      setQuiz(newQuiz);
-      console.log(newQuiz);
-      toastEmitted(["Quiz criado com sucesso!"], "success");
-    } else {
-      toastEmitted(["Erro ao criar quiz"], "warning");
-    }
-  }
-
-  function onDeletePergunta(pergunta: Pergunta) {
-    return () => {
-      const updatedPerguntas = providerTablePerguntas.filter(
-        (question) => question.pergId !== pergunta.pergId
-      );
-      setProviderTablePerguntas(updatedPerguntas);
-    };
-  }
-
-  return (
-    <div className="w-full px-14 flex flex-col gap-6 mt-6 h-full">
-      <div
-        className="toast toast-top toast-end z-50 cursor-pointer"
-        onClick={clearToast}
-      >
-        {toast.map((toast, index) => (
-          <div
-            key={index}
-            className={`alert ${toast.type == "warning" ? "alert-info" : ""} ${
-              toast.type == "success" ? "alert-success" : ""
-            }`}
-          >
-            <span>{toast.description}</span>
-          </div>
-        ))}
-      </div>
-      <div className="flex justify-between items-center">
-        <h1 className="text-2xl font-bold">Criar novo Quiz</h1>
-        <button className="btn btn-primary" onClick={onSubmit}>
-          Salvar
-        </button>
-      </div>
-      <div className="w-full flex justify-between align-center gap-12">
-        <div className="form-control w-full max-w-xl ">
-          <label className="label">
-            <span className="label-text">Título</span>
-          </label>
-          <input
-            value={quizTitle}
-            onChange={(e) => setQuizTitle(e.target.value)}
-            type="text"
-            className="input input-bordered w-full max-w-xl"
-          />
-        </div>
-        <div className="form-control w-full max-w-md self-end">
-          <input
-            value={quizImage}
-            onChange={(e) => setQuizImage(e.target.value)}
-            type="file"
-            className="file-input file-input-bordered w-full max-w-md"
-          />
-        </div>
-      </div>
-      <div className="form-control">
-        <label className="label">
-          <span className="label-text">Descrição</span>
-        </label>
-        <textarea
-          value={quizDescription}
-          onChange={(e) => setQuizDescription(e.target.value)}
-          className="textarea textarea-bordered h-24"
-        ></textarea>
-      </div>
-      <div className="flex flex-row gap-6">
-        <div className="form-control w-full">
-          <label className="label">
-            <span className="label-text">Pergunta</span>
-          </label>
-          <select
-            onChange={(e) => setPergunta(parseInt(e.target.value, 10))}
-            className="select select-bordered"
-            defaultValue="0"
-          >
-            <option value={0} disabled selected>
-              Selecione uma pergunta
-            </option>
-            {providerPerguntas.map((question) => (
-              <option value={question.pergId} key={question.pergId}>
-                {question.pergTittle}
-              </option>
-            ))}
-          </select>
-        </div>
-        <button className="btn btn-primary self-end" onClick={onAdd}>
-          Adicionar
-        </button>
-      </div>
-      <div className="overflow-auto">
-        <table className="table">
-          <thead>
-            <tr>
-              <th></th>
-              <th>Nome</th>
-              <th>Descrição</th>
-            </tr>
-          </thead>
-          <tbody>
-            {providerTablePerguntas.map((question) => (
-              <tr key={question.pergId}>
-                <th>{question.pergId}</th>
-                <td>{question.pergTittle}</td>
-                <td>{question.pergDescription}</td>
-                <th className="flex justify-end gap-2">
-                  <button
-                    className="btn btn-error btn-xs"
-                    onClick={onDeletePergunta(question)}
-                  >
-                    delete
-                  </button>
-                </th>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
 }
--- pages/home_quiz.tsx
+++ pages/home_quiz.tsx
@@ -1,143 +1,143 @@
 import Link from "next/link";
-import { AiOutlinePlusCircle } from "react-icons/ai";
+import {AiOutlinePlusCircle} from "react-icons/ai";
 import CardQuiz from "../components/card_quiz";
 
 let ProviderQuestionario: Questionario[] = getQuiz();
 ProviderQuestionario = ProviderQuestionario.concat(ProviderQuestionario);
 
 interface Questionario {
-  quesId: number;
-  quesTittle: string;
-  quesDescription: string;
-  quesImage: string;
-  perguntas: Perguntas[];
+    quesId: number;
+    quesTittle: string;
+    quesDescription: string;
+    quesImage: string;
+    perguntas: Perguntas[];
 }
 
 interface Perguntas {
-  pergId: number;
-  pergTittle: string;
-  pergDescription: string;
-  respostas: Resposta[];
+    pergId: number;
+    pergTittle: string;
+    pergDescription: string;
+    respostas: Resposta[];
 }
 
 interface Resposta {
-  respId: number;
-  respDescription: string;
-  respResposta: boolean;
+    respId: number;
+    respDescription: string;
+    respResposta: boolean;
 }
 
 function getQuiz() {
-  let quiz: Questionario[] = [];
+    let quiz: Questionario[] = [];
 
-  const quiz1 = {
-    quesId: 1,
-    quesTittle: "Quiz de teste 1",
-    quesDescription: "Quiz de teste para testar o quiz 1",
-    quesImage: "https://source.unsplash.com/random",
-    perguntas: [
-      {
-        pergId: 1,
-        pergTittle: "Primeira pergunta",
-        pergDescription: "Primeira pergunta",
-        respostas: [
-          {
-            respId: 1,
-            respDescription: "Primeira resposta",
-            respResposta: true,
-          },
-          {
-            respId: 2,
-            respDescription: "Segunda resposta",
-            respResposta: false,
-          },
+    const quiz1 = {
+        quesId: 1,
+        quesTittle: "Quiz de teste 1",
+        quesDescription: "Quiz de teste para testar o quiz 1",
+        quesImage: "https://source.unsplash.com/random",
+        perguntas: [
+            {
+                pergId: 1,
+                pergTittle: "Primeira pergunta",
+                pergDescription: "Primeira pergunta",
+                respostas: [
+                    {
+                        respId: 1,
+                        respDescription: "Primeira resposta",
+                        respResposta: true,
+                    },
+                    {
+                        respId: 2,
+                        respDescription: "Segunda resposta",
+                        respResposta: false,
+                    },
+                ],
+            },
+            {
+                pergId: 2,
+                pergTittle: "Segunda pergunta",
+                pergDescription: "Segunda pergunta",
+                respostas: [
+                    {
+                        respId: 1,
+                        respDescription: "Primeira resposta",
+                        respResposta: true,
+                    },
+                    {
+                        respId: 2,
+                        respDescription: "Segunda resposta",
+                        respResposta: false,
+                    },
+                ],
+            },
         ],
-      },
-      {
-        pergId: 2,
-        pergTittle: "Segunda pergunta",
-        pergDescription: "Segunda pergunta",
-        respostas: [
-          {
-            respId: 1,
-            respDescription: "Primeira resposta",
-            respResposta: true,
-          },
-          {
-            respId: 2,
-            respDescription: "Segunda resposta",
-            respResposta: false,
-          },
+    };
+
+    quiz.push(quiz1);
+
+    const quiz2 = {
+        quesId: 2,
+        quesTittle: "Quiz de teste 2",
+        quesDescription: "Quiz de teste para testar o quiz 2",
+        quesImage: "https://source.unsplash.com/random",
+        perguntas: [
+            {
+                pergId: 1,
+                pergTittle: "Primeira pergunta",
+                pergDescription: "Primeira pergunta",
+                respostas: [
+                    {
+                        respId: 1,
+                        respDescription: "Primeira resposta",
+                        respResposta: true,
+                    },
+                    {
+                        respId: 2,
+                        respDescription: "Segunda resposta",
+                        respResposta: false,
+                    },
+                ],
+            },
+            {
+                pergId: 2,
+                pergTittle: "Segunda pergunta",
+                pergDescription: "Segunda pergunta",
+                respostas: [
+                    {
+                        respId: 1,
+                        respDescription: "Primeira resposta",
+                        respResposta: true,
+                    },
+                    {
+                        respId: 2,
+                        respDescription: "Segunda resposta",
+                        respResposta: false,
+                    },
+                ],
+            },
         ],
-      },
-    ],
-  };
+    };
 
-  quiz.push(quiz1);
+    quiz.push(quiz2);
 
-  const quiz2 = {
-    quesId: 2,
-    quesTittle: "Quiz de teste 2",
-    quesDescription: "Quiz de teste para testar o quiz 2",
-    quesImage: "https://source.unsplash.com/random",
-    perguntas: [
-      {
-        pergId: 1,
-        pergTittle: "Primeira pergunta",
-        pergDescription: "Primeira pergunta",
-        respostas: [
-          {
-            respId: 1,
-            respDescription: "Primeira resposta",
-            respResposta: true,
-          },
-          {
-            respId: 2,
-            respDescription: "Segunda resposta",
-            respResposta: false,
-          },
-        ],
-      },
-      {
-        pergId: 2,
-        pergTittle: "Segunda pergunta",
-        pergDescription: "Segunda pergunta",
-        respostas: [
-          {
-            respId: 1,
-            respDescription: "Primeira resposta",
-            respResposta: true,
-          },
-          {
-            respId: 2,
-            respDescription: "Segunda resposta",
-            respResposta: false,
-          },
-        ],
-      },
-    ],
-  };
-
-  quiz.push(quiz2);
-
-  return quiz;
+    return quiz;
 }
 
 export default function HomeQuiz() {
-  return (
-    <div className="w-full px-12 flex">
-      <Link href="/create_quiz" legacyBehavior className="absolute">
-        <a>
-          <AiOutlinePlusCircle
-            style="color: #03A4FF"
-            className="w-10 h-10 absolute right-6 top-6 text-blue-500 cursor-pointer"
-          ></AiOutlinePlusCircle>
-        </a>
-      </Link>
-      <div className="w-full my-10 grid gap-y-12 gap-x-12 2xl:grid-cols-3 xl:grid-cols-2 justify-center">
-        {ProviderQuestionario.map((question, index) => (
-          <CardQuiz key={index} questionario={question} />
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div className="w-full px-12">
+            <Link href="/create_quiz"  className={"flex justify-end"}>
+
+                <AiOutlinePlusCircle
+                    style="color: #03A4FF"
+                    className="w-10 h-10 justify-end right-6 top-6 text-blue-500 cursor-pointer"
+                ></AiOutlinePlusCircle>
+
+            </Link>
+            <div className="w-full my-10 grid gap-y-12 gap-x-12 2xl:grid-cols-3 xl:grid-cols-2 justify-center">
+                {ProviderQuestionario.map((question, index) => (
+                    <CardQuiz key={index} questionario={question}/>
+                ))}
+            </div>
+        </div>
+    );
 }

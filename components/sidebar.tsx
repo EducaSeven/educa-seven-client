6c80d3314323b52b9c91a62d89e9e0dd3fc58f8f
Murilo Marçal de Araujo <102617690+Murilo-Araujo@users.noreply.github.com>
2023-12-04 00:12:24 -0300

    feat

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -44,6 +44,19 @@ export default function Sidebar({ children }: any) {
 									<span className="ml-3">Criar quiz</span>
 								</a>
 							</li>
+						<li>
+								<a
+									href="/create_question"
+									className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+								>
+									<MdQuiz
+										className={
+											"w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+										}
+									/>
+									<span className="ml-3">Criar pergunta</span>
+								</a>
+							</li>
 						</ul>
 					</div>
 				</aside>
